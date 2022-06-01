f045dba157db0c50fb6a2067ec09f0304d7abbae
Christian Marbach <christian@machro-coding.com>
2022-06-01 22:16:36 +0200

    Add basic routing methods

--- pkg/router.go
+++ pkg/router.go
@@ -9,14 +9,42 @@ type Router struct {
 	router httprouter.Router
 }
 
-func (r *Router) Get(path string, f func() string) {
-	r.router.GET(path, func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
-		writer.Write([]byte(f()))
-	})
-}
-
 func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	r.router.ServeHTTP(w, req)
+}
+
+func (r *Router) Get(path string, f func() string) {
+	r.handle(http.MethodGet, path, f)
+}
+
+func (r *Router) Post(path string, f func() string) {
+	r.handle(http.MethodPost, path, f)
+}
+
+func (r *Router) Put(path string, f func() string) {
+	r.handle(http.MethodPut, path, f)
+}
+
+func (r *Router) Patch(path string, f func() string) {
+	r.handle(http.MethodPatch, path, f)
+}
+
+func (r *Router) Delete(path string, f func() string) {
+	r.handle(http.MethodDelete, path, f)
+}
+
+func (r *Router) Options(path string, f func() string) {
+	r.handle(http.MethodOptions, path, f)
+}
+
+func (r *Router) Head(path string, f func() string) {
+	r.handle(http.MethodHead, path, f)
+}
+
+func (r *Router) handle(method string, path string, f func() string) {
+	r.router.Handle(method, path, func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+		writer.Write([]byte(f()))
+	})
 }
 
 func NewRouter() *Router {
--- test/router_test.go
+++ test/router_test.go
@@ -5,25 +5,41 @@ import (
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
 func TestBasic(t *testing.T) {
-	t.Run("Basic Get route should have the returned string of the callback in the body", func(t *testing.T) {
-		router := evoli.NewRouter()
-		router.Get("/test", func() string {
-			return "hello-world"
+	router := evoli.NewRouter()
+
+	methodTable := make(map[string]func(path string, f func() string))
+	methodTable[http.MethodGet] = router.Get
+	methodTable[http.MethodPost] = router.Post
+	methodTable[http.MethodPut] = router.Put
+	methodTable[http.MethodPatch] = router.Patch
+	methodTable[http.MethodDelete] = router.Delete
+	methodTable[http.MethodHead] = router.Head
+	methodTable[http.MethodOptions] = router.Options
+
+	for method, fn := range methodTable {
+		t.Run("Basic "+method+" route should have the returned string of the callback in the body", func(t *testing.T) {
+			path := "/" + strings.ToLower(method)
+
+			fn(path, func() string {
+				return "hello-world"
+			})
+
+			req, err := http.NewRequest(method, path, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			rr := httptest.NewRecorder()
+
+			router.ServeHTTP(rr, req)
+
+			assert.Exactly(t, "hello-world", rr.Body.String())
 		})
+	}
 
-		req, err := http.NewRequest(http.MethodGet, "/test", nil)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		rr := httptest.NewRecorder()
-
-		router.ServeHTTP(rr, req)
-
-		assert.Exactly(t, "hello-world", rr.Body.String())
-	})
 }
