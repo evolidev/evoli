c941fc6099d018221eff7e293de250d49d57e024
Christian Marbach <christian@machro-coding.com>
2022-06-19 15:22:06 +0200

    Add middlewares

---
+++ framework/router/middleware.go
@@ -0,0 +1,15 @@
+package router
+
+import "net/http"
+
+type MiddlewareFunc func(http.Handler) http.Handler
+
+// middleware interface is anything which implements a MiddlewareFunc named Middleware.
+type middleware interface {
+	Middleware(handler http.Handler) http.Handler
+}
+
+// Middleware allows MiddlewareFunc to implement the middleware interface.
+func (mw MiddlewareFunc) Middleware(handler http.Handler) http.Handler {
+	return mw(handler)
+}
