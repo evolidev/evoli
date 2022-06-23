27c28a0c2d7ecd8a646f2355a3bed5e29c5e65de
Christian Marbach <christian@machro-coding.com>
2022-06-23 16:22:53 +0200

    Add paths helper

---
+++ framework/use/paths.go
@@ -0,0 +1,73 @@
+package use
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"regexp"
+	"runtime"
+	"strings"
+)
+
+var rootDir string
+
+func isBuildAsRun() bool {
+	s := os.Args[0]
+	p := "go-build\\d+"
+
+	res, _ := regexp.MatchString(p, s)
+	fmt.Println("matching", res)
+	return res
+}
+
+func BasePath(...string) string {
+	if rootDir != "" {
+		return rootDir
+	}
+
+	if !isBuildAsRun() {
+		rootDir = getByExecutable()
+	} else {
+		rootDir = getByRuntime()
+	}
+
+	return rootDir
+}
+
+func getByExecutable() string {
+	filePath, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	rootDir = filepath.Dir(filePath)
+
+	return rootDir
+}
+
+func getByRuntime() string {
+	cnt := 0
+
+	for true {
+		_, b, _, ok := runtime.Caller(cnt)
+
+		if !ok {
+			break
+		}
+
+		tmp := path.Join(path.Dir(b))
+		res1 := strings.HasSuffix(tmp, "src/runtime")
+
+		if res1 {
+			cnt--
+
+			_, c, _, _ := runtime.Caller(cnt)
+			return path.Join(path.Dir(c))
+		}
+		cnt++
+	}
+
+	return ""
+}
