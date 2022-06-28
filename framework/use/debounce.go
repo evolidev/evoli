126a4c41e2f93639ca1214fceb582a317a73fb04
Omer Mohamed Ali <envomer@users.noreply.github.com>
2022-06-28 09:36:42 +0200

    Add debounce

---
+++ framework/use/debounce.go
@@ -0,0 +1,30 @@
+package use
+
+import (
+	"sync"
+	"time"
+)
+
+func Debounce(after time.Duration) func(f func()) {
+	d := &debouncer{after: after}
+
+	return func(f func()) {
+		d.add(f)
+	}
+}
+
+type debouncer struct {
+	mu    sync.Mutex
+	after time.Duration
+	timer *time.Timer
+}
+
+func (d *debouncer) add(f func()) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	if d.timer != nil {
+		d.timer.Stop()
+	}
+	d.timer = time.AfterFunc(d.after, f)
+}
