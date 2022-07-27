1e5747582a18ea0dcadb0b8ec8a574220a28d700
Christian Marbach <christian@machro-coding.com>
2022-07-27 18:01:56 +0200

    Use config in DB
    Also adding a migration command

--- framework/use/database.go
+++ framework/use/database.go
@@ -2,29 +2,53 @@ package use
 
 import (
 	"fmt"
+	"github.com/evolidev/evoli/framework/database"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"io/fs"
 	"os"
+	"strings"
 )
 
 var myDb *gorm.DB
+var migrations Collection[int, func(db *gorm.DB)]
+var migration *database.Migration
+
+func Migration() *database.Migration {
+	if nil == migration {
+		migration = database.NewMigrations()
+	}
+
+	return migration
+}
 
 func DB() *gorm.DB {
 	if nil != myDb {
 		return myDb
 	}
 
-	tmp := os.DirFS("/")
+	config := Config("db.sqlite.path")
 
-	_, err := fs.ReadDir(tmp, "database")
-	if err != nil {
-		err = os.Mkdir("database", 0755)
+	dir := config.Value().(string)
+
+	directories := strings.Split(dir, "/")
+	directories = directories[:len(directories)-1]
+
+	//todo get from storage
+	tmp := os.DirFS(BasePath())
+
+	for _, d := range directories {
+		_, err := fs.ReadDir(tmp, d)
 		if err != nil {
 			fmt.Println(err)
+			err = os.Mkdir(d, 0755)
+			if err != nil {
+				fmt.Println(err)
+			}
 		}
 	}
-	db, _ := gorm.Open(sqlite.Open("database/test.db"), &gorm.Config{})
+
+	db, _ := gorm.Open(sqlite.Open(dir), &gorm.Config{})
 
 	myDb = db
 
