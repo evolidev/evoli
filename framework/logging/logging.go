7e7d39b5b6ea3e402f6de90996a0cd391904c733
Omer Mohamed Ali <envomer@users.noreply.github.com>
2022-10-11 07:15:53 +0200

    Add plain logger

--- framework/logging/logging.go
+++ framework/logging/logging.go
@@ -20,23 +20,12 @@ const errorColor = 1
 const logColor = 61
 
 type Logger struct {
-	log    *log.Logger
-	config *Config
+	log      *log.Logger
+	plainLog *log.Logger
+	config   *Config
 }
 
 var Verbose = 0
-
-//func init() {
-//
-//	// log to console and file
-//	f, err := os.OpenFile("log.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-//	if err != nil {
-//		log.Fatalf("error opening file: %v", err)
-//	}
-//	wrt := io.MultiWriter(os.Stdout, f)
-//
-//	log.SetOutput(wrt)
-//}
 
 func NewLogger(c *Config) *Logger {
 	if c == nil {
@@ -45,9 +34,13 @@ func NewLogger(c *Config) *Logger {
 		}
 	}
 
-	var w = c.Stdout
-	if w == nil {
-		w = os.Stdout
+	var colorfulWriters []io.Writer
+	var plainWriters []io.Writer
+
+	if c.Stdout != nil {
+		colorfulWriters = append(colorfulWriters, c.Stdout)
+	} else {
+		colorfulWriters = append(colorfulWriters, os.Stdout)
 	}
 
 	if c.Location != "" {
@@ -60,13 +53,13 @@ func NewLogger(c *Config) *Logger {
 			log.Fatalf("error opening file: %v", err)
 		}
 
-		multiWriter := io.MultiWriter(w, f)
-		w = multiWriter
+		plainWriters = append(plainWriters, f)
 	}
 
 	return &Logger{
-		log:    log.New(w, "", 0),
-		config: c,
+		log:      log.New(io.MultiWriter(colorfulWriters...), "", 0),
+		plainLog: log.New(io.MultiWriter(plainWriters...), "", 0),
+		config:   c,
 	}
 }
 
@@ -80,54 +73,105 @@ func NewLoggerByName(name string, colorCode int) *Logger {
 func (l *Logger) getPrefix() string {
 	var prefixColor = l.config.PrefixColor
 	currentTime := time.Now()
+	prefix := ""
+
+	if l.config.Name != "" {
+		prefix = color.Text(prefixColor, "["+l.config.Name+"]") + " "
+	}
+
 	return fmt.Sprintf(
 		"%s %s",
 		color.Text(timeColor, currentTime.Format("2006-01-02 15:04:05")),
-		color.Text(prefixColor, "["+l.config.Name+"]"),
+		prefix,
+	)
+}
+
+func (l *Logger) getPlainPrefix() string {
+	currentTime := time.Now()
+	prefix := ""
+
+	if l.config.Name != "" {
+		prefix = "[" + l.config.Name + "]" + " "
+	}
+
+	return fmt.Sprintf(
+		"%s %s",
+		currentTime.Format("2006-01-02 15:04:05"),
+		prefix,
 	)
 }
 
 func (l *Logger) Log(msg interface{}, args ...interface{}) {
 	l.log.Printf(
-		fmt.Sprintf("%s %s %s", l.getPrefix(), color.Text(logColor, "Log"), color.Text(textColor, msg)),
+		fmt.Sprintf("%s%s %s", l.getPrefix(), color.Text(logColor, "Log"), color.Text(textColor, msg)),
+		args...,
+	)
+
+	l.plainLog.Printf(
+		fmt.Sprintf("%s%s %s", l.getPlainPrefix(), "Log", msg),
 		args...,
 	)
 }
 
 func (l *Logger) Info(msg interface{}, args ...interface{}) {
 	l.log.Printf(
-		fmt.Sprintf("%s %s %s", l.getPrefix(), color.Text(logColor, "Info"), color.Text(textColor, msg)),
+		fmt.Sprintf("%s%s %s", l.getPrefix(), color.Text(logColor, "Info"), color.Text(textColor, msg)),
+		args...,
+	)
+
+	l.plainLog.Printf(
+		fmt.Sprintf("%s%s %s", l.getPlainPrefix(), "Info", msg),
 		args...,
 	)
 }
 
 func (l *Logger) Success(msg interface{}, args ...interface{}) {
 	l.log.Printf(
-		fmt.Sprintf("%s %s %s", l.getPrefix(), color.Text(successColor, "Success"), color.Text(textColor, msg)),
+		fmt.Sprintf("%s%s %s", l.getPrefix(), color.Text(successColor, "Success"), color.Text(textColor, msg)),
+		args...,
+	)
+
+	l.plainLog.Printf(
+		fmt.Sprintf("%s%s %s", l.getPlainPrefix(), "Success", msg),
 		args...,
 	)
 }
 
 func (l *Logger) Error(msg interface{}, args ...interface{}) {
 	l.log.Printf(
-		fmt.Sprintf("%s %s %s", l.getPrefix(), color.Text(errorColor, "Error"), color.Text(textColor, msg)),
+		fmt.Sprintf("%s%s %s", l.getPrefix(), color.Text(errorColor, "Error"), color.Text(textColor, msg)),
+		args...,
+	)
+
+	l.plainLog.Printf(
+		fmt.Sprintf("%s%s %s", l.getPlainPrefix(), "Error", msg),
 		args...,
 	)
 }
 
 func (l *Logger) Debug(msg interface{}, args ...interface{}) {
 	l.log.Printf(
-		fmt.Sprintf("%s %s %s", l.getPrefix(), color.Text(debugColor, "Debug"), color.Text(textColor, msg)),
+		fmt.Sprintf("%s%s %s", l.getPrefix(), color.Text(debugColor, "Debug"), color.Text(textColor, msg)),
+		args...,
+	)
+
+	l.plainLog.Printf(
+		fmt.Sprintf("%s%s %s", l.getPlainPrefix(), "Debug", msg),
 		args...,
 	)
 }
 
 func (l *Logger) Print(msg interface{}, args ...interface{}) {
 	l.log.Printf(fmt.Sprintf(logFormat, color.Text(textColor, msg)), args...)
+
+	l.plainLog.Printf(fmt.Sprintf(logFormat, msg), args...)
 }
 
 func (l *Logger) Fatal(msg interface{}, args ...interface{}) {
 	l.log.Printf(fmt.Sprintf(logFormat, color.Text(textColor, msg)), args...)
+
+	l.plainLog.Printf(fmt.Sprintf(logFormat, msg), args...)
+
 	os.Exit(1)
 }
 
